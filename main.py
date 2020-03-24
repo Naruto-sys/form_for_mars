d39f602365b6c1b3bb76debfca4d9e218baa2ab5
Naruto-sys <gavriil.belov@yandex.ru>
2020-03-24 11:40:23 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import datetime
+
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+from data import db_session
+from data.users import User
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('E-mail', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    db_session.global_init("db/information.sqlite")
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User()
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        user.surname = form.surname.data
+        user.name = form.name.data
+        user.age = form.age.data
+        user.position = form.position.data
+        user.speciality = form.speciality.data
+        user.address = form.address.data
+        user.modifed_date = datetime.datetime.now()
+        session = db_session.create_session()
+        session.add(user)
+        session.commit()
+
+        return '<h1>Данные сохранены!</h1>'
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=7991, host='127.0.0.1')
